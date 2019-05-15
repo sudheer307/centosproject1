7e14d1c0741f07ff86878ac87781edea3ef92aec
sudheer307 <44992148+sudheer307@users.noreply.github.com>
2019-05-15 19:19:29 +0530

    Update App.java

--- src/main/java/project123/App.java
+++ src/main/java/project123/App.java
@@ -4,12 +4,4 @@ public class App {
 
         int num = 1234, reversed = 0;
 
-        while(num != 0) {
-            int digit = num % 10;
-            reversed = reversed * 10 + digit;
-            num /= 10;
-        }
-
-        System.out.println("Reversed Number: " + reversed);
-    }
-}
+        
